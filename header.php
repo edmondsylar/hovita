614a10f7ebf3dd667e2697703696b4e15efefd14
edmondsylar <edmondmusiitwa@gmail.com>
2020-03-21 09:12:36 -0700

    pushin first changes

---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>suubitech | Enterprises</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="" />
+    <meta name="author" content="http://webthemez.com" />
+    <!-- css -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="materialize/css/materialize.min.css" media="screen,projection" />
+    <link href="css/bootstrap.min.css" rel="stylesheet" />
+    <link href="css/fancybox/jquery.fancybox.css" rel="stylesheet">
+    <link href="css/flexslider.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+</head>
---
+++ index.php
@@ -0,0 +1,224 @@
+<?php include_once "header.php"; ?>
+<body>
+    <div id="wrapper" class="home-page">
+    <header class="topbar">
+            <div class="container">
+                <div class="row">
+                    <!-- social icon-->
+                    <div class="col-sm-3">
+                        <ul class="social-network">
+                            <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-pinterest"></i></a></li>
+                            <li><a class="waves-effect waves-dark" href="#"><i class="fa fa-google-plus"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- start header -->
+        <header>
+            <div class="navbar navbar-default navbar-static-top">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                        <a class="navbar-brand" href="index.html"><i class="icon-info-blocks material-icons">polymer</i>XTREAM</a>
+                    </div>
+                    <div class="navbar-collapse collapse ">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a class="waves-effect waves-dark" href="index.html">Home</a></li>
+                            <li class="dropdown">
+                                <a href="#" data-toggle="dropdown" class="dropdown-toggle waves-effect waves-dark">About Us <b class="caret"></b></a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="waves-effect waves-dark" href="about.html">Company</a></li>
+                                    <li><a class="waves-effect waves-dark" href="#">Our Team</a></li>
+                                    <li><a class="waves-effect waves-dark" href="#">News</a></li>
+                                    <li><a class="waves-effect waves-dark" href="#">Investors</a></li>
+                                </ul>
+                            </li>
+                            <li><a class="waves-effect waves-dark" href="services.html">Services</a></li>
+                            <li><a class="waves-effect waves-dark" href="contact.html">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- end header -->
+        <section id="banner">
+
+            <!-- Slider -->
+            <div id="main-slider" class="flexslider">
+                <ul class="slides">
+                    <li>
+                        <img src="img/slides/1.jpg" alt="" />
+                        <div class="flex-caption">
+                            <h3>Business Intellect</h3>
+                            <p>We create best opportunities</p>
+
+                        </div>
+                    </li>
+                    <li>
+                        <img src="img/slides/2.jpg" alt="" />
+                        <div class="flex-caption">
+                            <h3>Innovative Minds</h3>
+                            <p>Success depends on productivity</p>
+
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <!-- end slider -->
+        </section>
+        <section id="content">
+            <div class="container">
+
+                <section class="services">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="aligncenter">
+                                <h2 class="aligncenter">Our Services</h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, <br/>doloribus omnis minus ovident, doloribus omnis minus temporibus perferendis
+                                nesciunt..
+                            </div>
+                            <br/>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-4 info-blocks">
+                            <div class="info-blocks-in">
+                                <i class="icon-info-blocks material-icons">perm_identity</i>
+                                <h3>Consulting</h3>
+                                <div class="line"></div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 info-blocks">
+                            <div class="info-blocks-in">
+                                <i class="icon-info-blocks material-icons">card_travel</i>
+                                <h3>Finance</h3>
+                                <div class="line"></div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 info-blocks">
+                            <div class="info-blocks-in">
+                                <i class="icon-info-blocks material-icons">important_devices</i>
+                                <h3>Development</h3>
+                                <div class="line"></div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </section>
+        <section class="dishes">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="aligncenter">
+                            <h2 class="aligncenter">Our Process</h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, <br/>doloribus omnis minus ovident, doloribus omnis minus temporibus perferendis
+                            nesciunt..
+                        </div>
+                        <br/>
+                    </div>
+                </div>
+
+                <div class="row service-v1 margin-bottom-40">
+                    <div class="col-md-4 md-margin-bottom-40">
+                        <div class="card small">
+                            <div class="card-image">
+                                <img class="img-responsive" src="img/service1.jpg" alt="">
+                                <span class="card-title">Designing</span>
+                            </div>
+                            <div class="card-content">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 md-margin-bottom-40">
+                        <div class="card small">
+                            <div class="card-image">
+                                <img class="img-responsive" src="img/service2.jpg" alt="">
+                                <span class="card-title">Development</span>
+                            </div>
+                            <div class="card-content">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 md-margin-bottom-40">
+                        <div class="card small">
+                            <div class="card-image">
+                                <img class="img-responsive" src="img/service3.jpg" alt="">
+                                <span class="card-title">Quality</span>
+                            </div>
+                            <div class="card-content">
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt
+                                </p>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section class="section-padding gray-bg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-title text-center">
+                            <h2>Our Business</h2>
+                            <p>Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada Pellentesque <br>ipsum id orci porta dapibus. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6 col-sm-6">
+                        <div class="about-text">
+                            <p>Grids is a responsive Multipurpose Template. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta
+                                dapibus. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+
+                            <ul class="withArrow">
+                                <li><span class="fa fa-angle-right"></span> Lorem ipsum dolor sit amet</li>
+                                <li><span class="fa fa-angle-right"></span> consectetur adipiscing elit</li>
+                                <li><span class="fa fa-angle-right"></span> Curabitur aliquet quam id dui</li>
+                                <li><span class="fa fa-angle-right"></span> Donec sollicitudin molestie malesuada.</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary waves-effect waves-dark">Learn More</a>
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-6">
+                        <div class="about-image">
+                            <img src="img/about.jpg" alt="About Images">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+<?php include_once "footer.php"; ?>
