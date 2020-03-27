3b259ee1022730aa5d91ae4f7030a55de210c7be
edmondsylar <ukwelys@gmail.com>
2020-03-27 09:09:53 -0700

    modifications

--- index.php
+++ index.php
@@ -54,8 +54,8 @@
                                     <li><a class="waves-effect waves-dark" href="#">Investors</a></li>
                                 </ul>
                             </li>
-                            <li><a class="waves-effect waves-dark" disabled href="services.html">Services</a></li>
-                            <li><a class="waves-effect waves-dark" disabled href="contact.html">Contact</a></li>
+                            <li disabled><a class="waves-effect waves-dark" href="services.html">Services</a></li>
+                            <li disabled><a class="waves-effect waves-dark" href="contact.html">Contact</a></li>
                         </ul>
                     </div>
                 </div>
@@ -68,18 +68,18 @@
             <div id="main-slider" class="flexslider">
                 <ul class="slides">
                     <li>
-                        <img src="img/slides/1.jpg" alt="" />
+                        <img src="img/slides/i.png" alt="" />
                         <div class="flex-caption">
-                            <h3>Business Intellect</h3>
-                            <p>We create best opportunities</p>
+                            <h3>SME boosting.</h3>
+                            <p style="color: white;">Targeting small and Medium Enterprises all around the world, let be with you all the way.</p>
 
                         </div>
                     </li>
                     <li>
-                        <img src="img/slides/2.jpg" alt="" />
+                        <img src="img/slides/8.jpg" alt="" />
                         <div class="flex-caption">
-                            <h3>Innovative Minds</h3>
-                            <p>Success depends on productivity</p>
+                            <h3>Data analytics</h3>
+                            <p>We make sense of all the small bits to help you make the right business decisions.</p>
 
                         </div>
                     </li>
@@ -94,8 +94,12 @@
                     <div class="row">
                         <div class="col-md-12">
                             <div class="aligncenter">
-                                <h2 class="aligncenter">Our Services</h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, <br />doloribus omnis minus ovident, doloribus omnis minus temporibus perferendis
-                                nesciunt..
+                                <h2 class="aligncenter">Our Services</h2>
+                                We provide a wide reange of services aimed at growing start-up to maintaining and increasing efficienty in big organizations all around the globe
+                                <br> cutting edge technologies to ensure efficiency, security, customer reliability and <b>TRUST</b>.
+                                <br>
+                                <small>A more detailed list of all the services we offer can be found <a href="#">here</a>.</small>
+                                <!-- <small>A more detailed list of all the services we offer can be found <a href="services.php">here</a>.</small> -->
                             </div>
                             <br />
                         </div>
@@ -104,26 +108,26 @@
                     <div class="row">
                         <div class="col-sm-4 info-blocks">
                             <div class="info-blocks-in">
-                                <i class="icon-info-blocks material-icons">perm_identity</i>
-                                <h3>Consulting</h3>
+                                <i class="icon-info-blocks material-icons">language</i>
+                                <h3>Web Development</h3>
                                 <div class="line"></div>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt</p>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 info-blocks">
-                            <div class="info-blocks-in">
-                                <i class="icon-info-blocks material-icons">card_travel</i>
-                                <h3>Finance</h3>
-                                <div class="line"></div>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt</p>
+                                <p>From websites to web applications, we aim at letting the world know about your business and bridgre the gap between you and your clients</p>
                             </div>
                         </div>
                         <div class="col-sm-4 info-blocks">
                             <div class="info-blocks-in">
                                 <i class="icon-info-blocks material-icons">important_devices</i>
-                                <h3>Development</h3>
+                                <h3>Software Reselling</h3>
                                 <div class="line"></div>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt</p>
+                                <p>We have a varienty of softwares that are essential in both small and Big enterprises across the globe to ensure security, privacy and efficiency</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-4 info-blocks">
+                            <div class="info-blocks-in">
+                                <i class="icon-info-blocks material-icons">important_devices</i>
+                                <h3>Domain Management</h3>
+                                <div class="line"></div>
+                                <p>We take all the difficult work out of your hands, so you can focus ensuring that the services your customers are receiving as as good as your efforts dictate. </p>
                             </div>
                         </div>
                     </div>
@@ -136,29 +140,30 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="section-title text-center">
-                            <h2>Our Business</h2>
-                            <p>Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada Pellentesque <br>ipsum id orci porta dapibus. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                            <h2 style="text-transform: none;">.Know Us More.</h2>
+                            <p>suubitech is a not only a software company aimed at boosting business all around the world, Our dream is to transform the world using technology by providing Information that can be using in the Different aspect of our every day life, with our team of professionals that have dedicated there life and time to study the parteners of everything around us,</p>
                         </div>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col-md-6 col-sm-6">
                         <div class="about-text">
-                            <p>Grids is a responsive Multipurpose Template. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur aliquet quam id dui posuere blandit. Donec sollicitudin molestie malesuada. Pellentesque in ipsum id orci porta
-                                dapibus. Vivamus suscipit tortor eget felis porttitor volutpat.</p>
+                            <p>We aim to improve the living standards of people and solve the biggest world problems using this intel that may range from:</p>
 
                             <ul class="withArrow">
-                                <li><span class="fa fa-angle-right"></span> Lorem ipsum dolor sit amet</li>
-                                <li><span class="fa fa-angle-right"></span> consectetur adipiscing elit</li>
-                                <li><span class="fa fa-angle-right"></span> Curabitur aliquet quam id dui</li>
-                                <li><span class="fa fa-angle-right"></span> Donec sollicitudin molestie malesuada.</li>
+                                <li><span class="fa fa-angle-right"></span> Poverty</li>
+                                <li><span class="fa fa-angle-right"></span> Diseases</li>
+                                <li><span class="fa fa-angle-right"></span> Desertification </li>
+                                <li><span class="fa fa-angle-right"></span> Global warming/climate change.</li>
+                                <li><span class="fa fa-angle-right"></span> Nuclear holocaust</li>
                             </ul>
-                            <a href="#" class="btn btn-primary waves-effect waves-dark">Learn More</a>
+                            You can learn more about the <b>Afridata</b> program and or become apart of it here. <br>
+                            <a target="_blank" href="http://afridata.suubitech.com" class="btn btn-primary waves-effect waves-dark">Learn More</a>
                         </div>
                     </div>
                     <div class="col-md-6 col-sm-6">
                         <div class="about-image">
-                            <img src="img/about.jpg" alt="About Images">
+                            <img src="img/afri.jpg" alt="About Images">
                         </div>
                     </div>
                 </div>
